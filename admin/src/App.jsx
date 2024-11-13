31c8ee4d6c03d624a78028e208701da6af555fbe
Le-Thinh <lqt9112003@gmail.com>
2024-11-14 00:05:29 +0700

    Finish add product in FE Admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -35,9 +35,9 @@ const App = () => {
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<Add setToken={token} />} />
-                <Route path="/list" element={<List setToken={token} />} />
-                <Route path="/orders" element={<Orders setToken={token} />} />
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
               </Routes>
             </div>
           </div>
